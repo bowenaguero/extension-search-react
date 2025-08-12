56532c94a3a7be7cbc8f62bd0641531755c9c485
bowenaguero <bowenaguero@proton.me>
2025-08-12 08:25:16 -0400

    Refactor Home component layout and update Next.js dependency version

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import { ChevronsRight, Loader2, CircleAlert } from "lucide-react";
 import ExtensionIdTable from "@/components/table/ExtensionIdTable";
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(1)
   const [extensionIds, setExtensionIds] = useState<string[]>([]);
   const [extensionData, setExtensionData] = useState<{
     id: string;
@@ -16,8 +18,6 @@ export default function Home() {
     url: string;
     img_source: string
   }[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [progress, setProgress] = useState<number>(1)
 
   const handleChange = (text: string) => {
     const extensionIds = getExtensionIds(text);
@@ -36,7 +36,7 @@ export default function Home() {
     setLoading(true);
     setExtensionData([]);
     setProgress(1);
-    
+
     const fetches: Promise<unknown>[] = [];
     extensionIds.map(async (id) => {
       fetches.push(fetch("/api/get_extension_data", {
@@ -61,74 +61,63 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <div className="flex flex-col gap-18 h-200 justify-center items-center">
-        <div className="gap-2 flex flex-col h-full items-center">
-          <h1 className="text-5xl font-bold">Browser Extension Search</h1>
-          <p className="text-gray-500 text-lg">Identify browser extensions by ID.</p>
-        </div>
-        <div className="flex h-full w-full gap-12">
-          <div className="flex flex-col gap-8 border border-gray-300 rounded-sm p-8 w-150 shadow-lg">
-            <div className="flex flex-col gap-2">
-              <h1 className="text-4xl font-bold">Extension IDs</h1>
-              <p className="text-gray-500">
-                Paste text containing extension IDs.
-              </p>
-            </div>
-            <div className="flex flex-col gap-4 w-full">
-              <div className="flex flex-row pb-6">
-                <Textarea
-                  className="border border-gray-300 rounded-sm p-2 w-full h-140 resize-none"
-                  id="extensionIds"
-                  placeholder="Enter extension IDs"
-                  onChange={(e) => {
-                    handleChange(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="flex flex-row gap-4">
-                <button
-                  className="rounded-sm p-2 hover:bg-gray-900 bg-black text-white font-medium hover:cursor-pointer w-full"
-                  onClick={() => handleSubmit()}
-                >
-                  Submit
-                </button>
-                <button
-                  className="rounded-sm p-2 hover:bg-gray-100 bg-white border border-gray-300 text-black font-medium hover:cursor-pointer w-full"
-                  onClick={() => handleClear()}
-                >
-                  Clear
-                </button>
-              </div>
+    <div className="flex flex-col items-center justify-center h-screen p-12 font-sans">
+      <div className="gap-2 flex h-[25%] flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold">Browser Extension Search</h1>
+      </div>
+      <div className="flex flex-row flex-grow h-[75%] w-full items-center justify-center gap-8">
+        <div className="flex flex-col w-[35%] h-full border border-gray-300 rounded-sm shadow-lg p-8 gap-4">
+          <div className="flex flex-col h-full gap-4">
+            <Textarea
+              className="h-full border border-gray-300 rounded-sm resize-none"
+              id="extensionIds"
+              placeholder="Paste your extensions IDs here"
+              onChange={(e) => {
+                handleChange(e.target.value);
+              }}
+            />
+            <div className="flex flex-row gap-4">
+              <button
+                className="rounded-sm hover:bg-gray-900 bg-black text-white font-medium hover:cursor-pointer w-full"
+                onClick={() => handleSubmit()}
+              >
+                Submit
+              </button>
+              <button
+                className="rounded-sm hover:bg-gray-100 bg-white border border-gray-300 text-black font-medium hover:cursor-pointer w-full"
+                onClick={() => handleClear()}
+              >
+                Clear
+              </button>
             </div>
           </div>
-          <div className="flex h-full items-center">
-            {loading ? (
-              <div className="flex flex-col items-center justify-center gap-2">
-                <Loader2 className="h-10 w-10 animate-spin" />
-                <p className="">{extensionIds[progress]}</p>
-                <p className="text-gray-500">{progress}/{extensionIds.length}</p>
-              </div>
-            ) : (
-              <div className="flex flex-col items-center justify-center gap-2">
+        </div>
+        <div className="flex items-center">
+          {loading ? (
+            <div className="flex flex-col items-center justify-center gap-2">
+              <Loader2 className="h-10 w-10 animate-spin" />
+              <p className="">{extensionIds[progress]}</p>
+              <p className="text-gray-500">{progress}/{extensionIds.length}</p>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center gap-2">
               <ChevronsRight className="h-10 w-10" />
               <p className="text-sm font-medium">{extensionIds.length} Loaded</p>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col h-full w-[35%] border border-gray-300 rounded-sm shadow-lg">
+          <div className="flex flex-col h-full w-full px-0 items-center justify-center">
+            {extensionData.length > 0 ? (
+              <ExtensionIdTable extensionData={extensionData} />
+            ) : (
+              <div className="flex gap-1 items-center">
+                <CircleAlert className="w-4 h-4 font-medium" />
+                <p className="text-sm font-medium">
+                  Upload extension IDs to view results.
+                </p>
               </div>
             )}
-          </div>
-          <div className="flex flex-col gap-8 border border-gray-300 rounded-sm p-8 w-150 h-full shadow-lg">
-            <div className="flex flex-col h-full w-full px-0 items-center justify-center">
-              {extensionData.length > 0 ? (
-                <ExtensionIdTable extensionData={extensionData} />
-              ) : (
-                <div className="flex gap-1 items-center">
-                  <CircleAlert className="w-4 h-4 font-medium" />
-                  <p className="text-sm font-medium">
-                    Upload extension IDs to view results.
-                  </p>
-                </div>
-              )}
-            </div>
           </div>
         </div>
       </div>
