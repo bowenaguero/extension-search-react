92b16b3f85b8f8de6ab7aae02c564d8f744b43ad
bowenaguero <bowenaguero@proton.me>
2025-08-12 10:43:56 -0400

    Remove unnecessary table headers and improve cell alignment in ExtensionIdTable component

--- components/table/ExtensionIdTable.tsx
+++ components/table/ExtensionIdTable.tsx
@@ -25,19 +25,12 @@ export default function ExtensionIdTable({
   return (
     <div className="overflow-y-scroll w-full h-full scrollbar-hide">
       <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-left w-1/10 font-bold"></TableHead>
-            <TableHead className="text-left w-5/10 font-medium">ID</TableHead>
-            <TableHead className="text-left w-4/10 font-medium">Name</TableHead>
-          </TableRow>
-        </TableHeader>
         <TableBody>
           {extensionData.map((data, index) => (
             <TableRow key={index}>
               {data.found ? (
                 <>
-                  <TableCell className="text-left">
+                  <TableCell className="flex items-center justify-center">
                     <Image
                       src={data.img_source}
                       alt={data.browser}
